28122e973786acbea63d994a47f885aae3d1164e
Rishikesh Bade <108173199+Rishikeshbade@users.noreply.github.com>
2025-05-29 13:04:47 +0530

    Initial Commit
    Yes

--- src/Threads/JoinExample.java
+++ src/Threads/JoinExample.java
@@ -1,6 +1,19 @@
 package Threads;
 
-class Join extends Thread {
+class Join1 extends Thread {
+    public void run() {
+        for (int i = 1; i < 5; i++) {
+            try {
+                Thread.sleep(500);
+                System.out.println("Running thread: " + Thread.currentThread().getName());
+            } catch (Exception e) {
+                System.out.println("Thread is Interrupted!! ");
+            }
+        }
+    }
+}
+
+class Join2 extends Thread {
     public void run() {
         for (int i = 1; i < 5; i++) {
             try {
@@ -17,12 +30,18 @@ public class JoinExample {
 
     public static void main(String[] args) {
 
-        Join join = new Join();
+        Join1 join = new Join1();
         join.setName("Thread-1");
-        join.start(); 
+        
+        Join2 jn = new Join2();
+        jn.setName("Thread-2");
+        
+        join.start();
+        jn.start();
 
         try {
-            join.join();  
+            join.join(); 
+            jn.join();
         } catch (Exception e) {
             System.out.println("Main Thread is Interrupted!!");
         }
